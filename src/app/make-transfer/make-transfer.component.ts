8308795493e6683ceb5369b71dedd5696c49ec40
rwitczyk <r.witczyk@wp.pl>
2019-07-10 14:24:03 -0400

    Changes in front in MakeTransfer Component

--- src/app/make-transfer/make-transfer.component.ts
+++ src/app/make-transfer/make-transfer.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {CreateTransferService} from '../Services/create-transfer.service';
 import {Transfer} from '../Models/Transfer';
+import {ListOfAccountsService} from '../Services/list-of-accounts.service';
+import {BankAccount} from '../Models/BankAccount';
 
 @Component({
   selector: 'app-make-transfer',
@@ -9,16 +11,25 @@ import {Transfer} from '../Models/Transfer';
 })
 export class MakeTransferComponent implements OnInit {
   private newTransfer: Transfer;
-  constructor(private createTransferService: CreateTransferService) {
+  private accounts: BankAccount[];
+  private listOfNumberAccounts: string[] = [];
+  constructor(private createTransferService: CreateTransferService, private listOfAccountsService: ListOfAccountsService) {
   }
 
   ngOnInit() {
+    this.listOfAccountsService.getListOfAccounts().subscribe((res: BankAccount[]) => {
+      this.accounts = res;
+      for (let i = 0; i < this.accounts.length; i++) {
+        this.listOfNumberAccounts[i] = this.accounts[i].numberAccount;
+        console.log(this.accounts[i].numberAccount);
+      }
+    });
   }
 
-  createNewTransfer(fromNumberAccount: string, toNumberAccout: string, balance: string) {
+  createNewTransfer(fromNumberAccount: string, toNumberAccount: string, balance: string) {
     this.newTransfer = new Transfer();
     this.newTransfer.fromNumberAccount = fromNumberAccount;
-    this.newTransfer.toNumberAccount = toNumberAccout;
+    this.newTransfer.toNumberAccount = toNumberAccount;
     this.newTransfer.balance = balance;
     this.createTransferService.createTransfer(this.newTransfer);
   }
