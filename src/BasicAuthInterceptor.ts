393543009b931034953dd4617b232857341558d9
rwitczyk <r.witczyk@wp.pl>
2019-07-25 16:49:51 -0400

    changed in interceptor

--- src/BasicAuthInterceptor.ts
+++ src/BasicAuthInterceptor.ts
@@ -13,9 +13,12 @@ export class BasicAuthInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     request = request.clone({
       setHeaders: {
+        ContentType: 'application/json',
         Authorization: 'Basic ' + btoa(this.login + ':' + this.password),
         AccessControlAllowMethods: 'POST, GET, OPTIONS, PUT, DELETE',
-        AccessControlAllowOrigin: 'https://bankrobloxback.herokuapp.com/'
+        'Access-Control-Allow-Origin': '*',
+        AccessControlAllowHeaders: 'Access-Control-Allow-Origin, Content-Type, Accept, Accept-Language, Origin, User-Agent',
+        AccessControlAllowCredentials: 'true'
       }
     });
 
